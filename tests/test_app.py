333aae48c0b265acc5d84068d85a532aadea4f68
mcorvax <mcorvax@gmail.com>
2025-01-02 16:23:43 +0200

    initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(app.add(1, 2), 3)
+        self.assertEqual(app.add(1, -2), -1)
+        self.assertEqual(app.add(-1, -2), -3)
+        self.assertEqual(app.add(0, 0), 0)
+        
+if __name__ == '__main__':
+    unittest.main()
